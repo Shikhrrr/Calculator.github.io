f5f5ccc5bbdec54abc271c024746b66a48507f6b
shikhrrr <shikhar20396.dpskalyanpur@gmail.com>
2024-01-26 16:50:02 +0530

    added CE button, disabled input

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
     let displayValue = "";
     let buttons = document.querySelectorAll('.button');
-
+    document.querySelector('input').disabled = true;
+    
     Array.from(buttons).forEach (function (button) {
         button.addEventListener('click', function (elem) {
-
+            
             if (elem.target.innerHTML == '=') {
                 displayValue = eval(displayValue);
                 document.querySelector('input').value = displayValue;
@@ -14,9 +15,20 @@
                 document.querySelector('input').value = displayValue;
             }
             
+            else if (elem.target.innerHTML == 'CE') {
+                // let len = displayValue.length;
+                // displayValue /= 10;
+                // displayValue = parseInt(displayValue);
+                // displayValue = displayValue.toString();
+                // displayValue -= displayValue[len];
+
+                displayValue = displayValue.slice(0, -1);
+                document.querySelector('input').value = displayValue;
+            }
+
             else {
                 displayValue = displayValue + elem.target.innerHTML;
                 document.querySelector('input').value = displayValue;
             }
-        });
+        });     
 });
